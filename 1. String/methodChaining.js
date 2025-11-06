9a6798f7776192b5bcc9947fa7065cef12648079
DheerajSharma0328 <DheerajSharma0328@gmail.com>
2025-11-07 03:12:40 +0530

    Method Chaining

---
+++ 1. String/methodChaining.js
@@ -0,0 +1,9 @@
+/**
+ * 1=> What is Method Chaining
+ * Ans=> When we use one method after another. Execution start from left to right
+ *  
+ */
+
+let mess = " dheeraj "
+
+console.log(mess.trim().toUpperCase());
