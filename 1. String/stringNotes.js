31a57e3ad26648dae4d928b90fac14aba6f63404
DheerajSharma0328 <DheerajSharma0328@gmail.com>
2025-11-07 03:10:15 +0530

    String Notes

--- 1. String/stringNotes.js
+++ 1. String/stringNotes.js
@@ -1,5 +1,5 @@
 /**
  * 1=> Strings are Immutable in js
  * 2=> Immutable mean when we try to make change in string , a new string is created and original remain same
- * 3=> 
+ * 3=> String are zero based index mean counting start from 0 not from 1 
  */
