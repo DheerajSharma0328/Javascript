b3357e1bf674f0739a9adc906862d0ef541a674a
DheerajSharma0328 <DheerajSharma0328@gmail.com>
2025-11-09 19:27:00 +0530

    Call Back Complete

--- 5. Asynchronous JavaScript/callBack.js
+++ 5. Asynchronous JavaScript/callBack.js
@@ -1,6 +1,11 @@
 /**
  * 1=> What is call back
- * Ans=> When we pass a function to another function as arguments
+ * Ans=> When we pass a function to another function as arguments then it is called callback
+ *    => In javascript fuction are treated as First-Class_object meaning they can be treated like any other data type
+ *       (assigned to variables, passed as arguments, returned from functions).
+ * 
+ * 2=> What is call back hell
+ * Ans=> When we do nesting of the call back then this is called call back hell (like below)
  */
 
 const h1 = document.querySelector('h1')
@@ -15,6 +20,8 @@ function setColor(shades, delay, newColor){
 
 }
 
+
+// call back hell
 setColor("blue",3000, ()=>{
     setColor("pink",3000, ()=>{
         setColor("orange",3000)
