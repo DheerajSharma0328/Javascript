69ba5b5a7b66de1cb01661be3df5e9e546c43e01
DheerajSharma0328 <DheerajSharma0328@gmail.com>
2026-05-05 16:42:51 +0530

    Tips for writing Clean Code

---
+++ 1. The Javascript Language/1. JavaScript Fundamentals/2. Code structure/code_structure.js
@@ -0,0 +1,6 @@
+console.log("Use Camel Case in Javascript");
+console.log("Use Comment");
+console.log("Make Variable Logical Name");
+console.log("Don't Repeat the same code Again And Again");
+console.log("Break the code as much as possible in the form of Function");
+console.log("Make one function for one task");
