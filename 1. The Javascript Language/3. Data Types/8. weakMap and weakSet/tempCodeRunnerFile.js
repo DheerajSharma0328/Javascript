8e9affb3ce0dcfd37140486b937eabddd13ffba0
dheerajsharma0328 <dheerajsharma0328@gmail.com>
2026-05-14 18:57:00 +0530

    Complete weakMap and Weak set

---
+++ 1. The Javascript Language/3. Data Types/8. weakMap and weakSet/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+let john = { name: "John" };
+
+let map = new Map();
+map.set(john, "...");
+
+john = null; // overwrite the reference
+console.log("Map Key => ",map.keys());
--- 1. The Javascript Language/3. Data Types/8. weakMap and weakSet/weakMap.js
+++ 1. The Javascript Language/3. Data Types/8. weakMap and weakSet/weakMap.js
@@ -1,29 +1,56 @@
-// Reference of object in map after assign object variable to null
+// Object reference concept 
 
+let obj = {name : "Dheeraj"};
+let arr = [obj];
+console.log("One" , arr[0]);
+obj = null;
+console.log("Two" , arr[0]);
+console.log(obj);
+
+//------------------------------------------------------------------------------------------
+
+//if we use an object as the key in a regular Map, then while the Map exists, that object exists as well. It occupies memory and may not be garbage collected.
+
+let john = { name: "John" };
 
 let map = new Map();
-let obj = {name : 'Jhon'};
+map.set(john, "...");
 
-map.set(obj , 'Object Data');
-// console.log(map);
-obj = null;
-// console.log('Map Ater obj = null:  ' , map);
-console.log('object key data' , map.get(null));
+john = null; // overwrite the reference
+console.log("Map Key => ",map.keys());
+
+// john is stored inside the map,
+// we can get it by using map.keys()
 
 
+//------------------------------------------------------------------------------------------
 
-// let weakMap = new WeakMap();
-// let obj1 = {name : 'Jhon'};
+// The first difference between Map and WeakMap is that keys must be objects, not primitive values:
 
-// weakMap.set(obj1 , 'ok'); // work fine because object is the key
+let weakMap = new WeakMap();
 
-// //can't use string as the key
-// // weakMap.set('Alpaha' , 'A'); 
-// // Error, because "test" is not an object
+let obj1 = {};
 
-// obj1 = null;
+weakMap.set(obj1, "ok"); // works fine (object key)
 
-// console.log(weakMap.get(obj1));
-// console.log(weakMap);
+// can't use a string as the key
+// weakMap.set("test", "Whoops"); 
+// Error, because "test" is not an object
 
-console.log("Working");
+//------------------------------------------------------------------------------------------
+
+//Now, if we use an object as the key in it, and there are no other references to that object – it will be removed from memory (and from the map) automatically.
+
+let john1 = { name: "John" };
+
+let weakRef = new WeakMap();
+weakRef.set(john1, "...");
+
+console.log("Before null weak map test: " , weakRef.has(john1));
+
+john1 = null; // overwrite the reference
+
+console.log("Before null weak map test: " , weakRef.has(john1));
+// john is removed from memory!
+
+//------------------------------------------------------------------------------------------
