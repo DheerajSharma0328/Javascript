15132cabdc80c7bcc7db6359fbb2693a501d1fc4
dheerajsharma0328 <dheerajsharma0328@gmail.com>
2026-05-28 15:56:43 +0530

    return function inside of a funcion

--- 1. The Javascript Language/4. Advance working with function/03. Variable Scope , Closure/nestedFuncton.js
+++ 1. The Javascript Language/4. Advance working with function/03. Variable Scope , Closure/nestedFuncton.js
@@ -1,9 +1,10 @@
 function outer(firstName){
 
     inner("Sharma");
-    // console.log(lastname);
+    console.log(lastname1);
 
     function inner(lastname){
+        // let lastname1 = lastname;
         console.log("Full Name: " , firstName , lastname);
     }
 
@@ -11,3 +12,18 @@ function outer(firstName){
 
 
 outer("Dheeraj");
+
+// We can also return the function
+
+function makeCounter() {
+  let count = 0;
+
+  return function() {
+    return count++;
+  };
+}
+
+let counter = makeCounter();
+
+console.log( counter() ); // 0
+console.log( counter() ); // 1
