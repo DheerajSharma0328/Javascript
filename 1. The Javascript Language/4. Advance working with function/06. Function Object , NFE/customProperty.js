8e396e4d0398a392b00451cb4b26e648e59b45a3
dheerajsharma0328 <dheerajsharma0328@gmail.com>
2026-06-05 23:22:22 +0530

    Custom Property

---
+++ 1. The Javascript Language/4. Advance working with function/06. Function Object , NFE/customProperty.js
@@ -0,0 +1,12 @@
+function sayHi() {
+  console.log("Hi");
+
+  // let's count how many times we run
+  sayHi.counter++;
+}
+sayHi.counter = 0; // initial value
+
+sayHi(); // Hi
+sayHi(); // Hi
+
+console.log( `Called ${sayHi.counter} times` ); // Called 2 times
