e3d7532104f29bd521230d3f163e0c69458cfd9e
dheerajsharma0328 <dheerajsharma0328@gmail.com>
2026-06-05 23:17:39 +0530

    Function Property

---
+++ 1. The Javascript Language/4. Advance working with function/06. Function Object , NFE/nameProperty.js
@@ -0,0 +1,6 @@
+function sayHi() {
+  console.log("Hi");
+}
+
+console.log("Calling the Function and Printing Function Body:" , sayHi());
+console.log("Printing Function Name: " , sayHi.name); // sayHi
