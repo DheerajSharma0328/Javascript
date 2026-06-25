f8477c455e2611a0d92d02bdc799c040d6b04f59
dheerajsharma0328 <dheerajsharma0328@gmail.com>
2026-06-25 16:44:20 +0530

    add 0 before every number in folder name , add code in global object

---
+++ 1. The Javascript Language/04. Advance working with function/05. Global Object/global.js
@@ -0,0 +1,32 @@
+// All properties of the global object can be accessed directly:
+
+console.log("Hello");
+// is the same as
+// window.console.log("Hello");
+globalThis.console.log("Hello");
+
+alert("Hello");
+// is the same as
+window.alert("Hello");
+
+var gVar = 5;
+
+alert(window.gVar); //5 (became a property of the global object)
+
+let gLet = 5;
+
+alert(window.gLet); // undefined (doesn't become a property of the global object)
+
+//If a value is so important that you’d like to make it available globally, write it directly as a property:
+
+// make current user information global, to let all scripts access it
+window.currentUser = {
+  name: "John"
+};
+
+// somewhere else in code
+alert(currentUser.name);  // John
+
+// or, if we have a local variable with the name "currentUser"
+// get it from window explicitly (safe!)
+alert(window.currentUser.name); // John
--- 1. The Javascript Language/4. Advance working with function/05. Global Object/global.js
+++
@@ -1,6 +0,0 @@
-// All properties of the global object can be accessed directly:
-
-console.log("Hello");
-// is the same as
-// window.console.log("Hello");
-globalThis.console.log("Hello");
